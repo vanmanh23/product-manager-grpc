405ddb9b8b030760df0387246f78bd998a720f1a
vanmanh23 <manhcan03@gmail.com>
2026-04-20 22:32:40 +0700

    initialize project with base gRPC structure (WIP)

---
+++ productManager_Server/src/main/java/com/beanannotation/srpc/server/Main.java
@@ -0,0 +1,7 @@
+package com.beanannotation.srpc.server;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ productManager_Server/src/main/java/com/beanannotation/srpc/server/service/ProductServerService.java
@@ -0,0 +1,7 @@
+package com.beanannotation.srpc.server.service;
+
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class ProductServerService {
+}
